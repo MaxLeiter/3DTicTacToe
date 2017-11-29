203df427c66cec67fb1f1d773147515e83d1c52b
Max Leiter <mleiter18@priorypanther.com>
2017-11-29 14:20:33 -0800

    Alpha-beta pruning

--- Game.java
+++ Game.java
@@ -4,13 +4,14 @@ public class Game {
 	private Board.Tile ComputerTile = Board.Tile.X; // TODO: Set in constructor when you start game
 	private Board.Tile AdversaryTile = Board.Tile.O; // TODO: Set in constructor when you start game
 	private static boolean debug = false;
+
 	public static void main(String[] args) {
 		Game game = new Game();
 		try {
-			Coordinate x = game.start(Board.Tile.X, 3);
-			//if (debug) {
-				System.out.printf("board[%d][%d][%d] = Tile.X", x.getX(), x.getY(), x.getZ());
-			//}
+			Coordinate x = game.start(Board.Tile.X, 4);
+			// if (debug) {
+			System.out.printf("board[%d][%d][%d] = Tile.X", x.getX(), x.getY(), x.getZ());
+			// }
 		} catch (Board.InvalidMoveException e) {
 			e.printStackTrace();
 		}
@@ -21,10 +22,11 @@ public class Game {
 		Coordinate bestMove = null;
 		Board board = new Board();
 		List<Coordinate> moves = board.getValidMoves();
+		double alpha = Double.NEGATIVE_INFINITY;
+		double beta = Double.POSITIVE_INFINITY;
 		for (Coordinate move : moves) {
-			System.out.println("We going");
 			Board temp = board.move(move, turn);
-			double score = minimax(getNextTile(turn), temp, maxDepth);
+			double score = minimax(getNextTile(turn), temp, maxDepth, alpha, beta);
 			if (debug) {
 				System.out.println("score: " + score);
 				System.out.println("bestScore: " + bestScore);
@@ -44,19 +46,19 @@ public class Game {
 	}
 
 	/**
-	 * Returns the best move to be made
+	 * Returns the best move to be made using alpha-beta
 	 * 
 	 * @param tile
 	 * @param board
 	 * @param depth
-	 * @return
+	 * @return utility value
 	 */
-	public double minimax(Board.Tile tile, Board board, int maxDepth) {
+	public double minimax(Board.Tile tile, Board board, int maxDepth, double alpha, double beta) {
 		double score = 0;
 		if (tile == ComputerTile) {
-			score = max(tile, board, maxDepth);
+			score = max(tile, board, maxDepth, alpha, beta);
 		} else {
-			score = min(tile, board, maxDepth);
+			score = min(tile, board, maxDepth, alpha, beta);
 		}
 		return score;
 	}
@@ -65,36 +67,42 @@ public class Game {
 	 * Returns utility value. Minimize opponent.
 	 * 
 	 * @param board
-	 * @return
+	 * @return utility value
 	 */
 
-	private double min(Board.Tile tile, Board board, int depth) {
+	private double min(Board.Tile tile, Board board, int depth, double alpha, double beta) {
 		double eval = board.evaluate();
+
 		if (Double.isInfinite(eval) || depth <= 0) {
 			return eval;
 		}
+
 		List<Coordinate> moves = board.getValidMoves();
 
 		double lowestMax = Double.POSITIVE_INFINITY; // need to maximize this
-		Coordinate bestMaxMove = null;
 		for (int index = 0; index < moves.size(); index++) {
 			try {
 				Coordinate move = moves.get(index);
 				Board temp = board.move(move, tile);
-				double max = max(getNextTile(tile), temp, depth - 1);
+				double max = max(getNextTile(tile), temp, depth - 1, alpha, beta);
 				if (max < lowestMax) {
 					lowestMax = max;
-					bestMaxMove = move;
 				}
-			} catch (Exception e) {
+				if (lowestMax <= alpha) {
+					return lowestMax;
+				}
+				beta = Math.min(beta, lowestMax);
+			} catch (Board.InvalidMoveException e) {
 				e.printStackTrace();
 			}
 		}
+
 		return lowestMax;
 	}
 
-	private double max(Board.Tile tile, Board board, int depth) {
+	private double max(Board.Tile tile, Board board, int depth, double alpha, double beta) {
 		double eval = board.evaluate();
+
 		if (Double.isInfinite(eval) || depth <= 0) {
 			return eval;
 		}
@@ -102,20 +110,26 @@ public class Game {
 		List<Coordinate> moves = board.getValidMoves();
 
 		double largestMin = Double.NEGATIVE_INFINITY; // need to minimize this
-		Coordinate bestMinMove = null;
+		Coordinate bestMinMove = null; // for debugging purposes
+
 		for (int index = 0; index < moves.size(); index++) {
 			try {
 				Coordinate move = moves.get(index);
 				Board temp = board.move(move, tile);
-				double min = min(getNextTile(tile), temp, depth - 1);
-				if (min > largestMin) {
+				double min = min(getNextTile(tile), temp, depth - 1, alpha, beta);
+				if (min > largestMin) { // largestMin = Math.max(largestMin, min);
 					largestMin = min;
 					bestMinMove = move;
 				}
-			} catch (Exception e) {
+				if (largestMin >= beta) {
+					return largestMin;
+				}
+				alpha = Math.max(alpha, largestMin);
+			} catch (Board.InvalidMoveException e) {
 				e.printStackTrace();
 			}
 		}
+
 		if (debug) {
 			System.out.println("coords: " + bestMinMove);
 		}
